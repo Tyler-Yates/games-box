da4aad536c44be1285835971deeeeae4fbf42743
Tyler <5478568+Tyler-Yates@users.noreply.github.com>
2024-02-11 18:38:46 -0600

    Attempt CORS fix

--- application/__init__.py
+++ application/__init__.py
@@ -13,7 +13,7 @@ SCRAMBLED_WORDS_GAME_MANAGER_CONFIG_KEY = "sw_game_manager"
 HIDDEN_NAMES_GAME_MANAGER_CONFIG_KEY = "hn_game_manager"
 CROSSWORD_CREATOR_GAME_MANAGER_CONFIG_KEY = "cc_game_manager"
 
-socketio = SocketIO()
+socketio = SocketIO(cors_allowed_origins="*")
 
 LOG = logging.getLogger("__init__")
 logging.basicConfig(level=logging.INFO)
